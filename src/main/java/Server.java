b1d41b1a5dc38396b45b1b923f8d8c260b544ffd
Dhairya Doshi <doshid@purdue.edu>
2016-09-22 09:16:09 -0400

    Completed inputing files from user

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,18 +1,18 @@
-import java.io.File;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by Dhairya on 9/20/2016.
  */
 public class Server implements Runnable {
     int portNumber;
-    SocketWrapper socketWrapper = null;
+    private SocketWrapper socketWrapper = null;
+    private ArrayList<String> fileLocationList;
 
     public Server() {
         portNumber = 9125;
+        fileLocationList = new ArrayList<String>();
     }
 
     public Server(int portNumber) {
@@ -22,11 +22,64 @@ public class Server implements Runnable {
         else {
             this.portNumber = portNumber;
         }
+        fileLocationList = new ArrayList<String>();
     }
 
-    public ArrayList<File> getInputFiles() {
-        // TODO: Get files from user
+    public ArrayList<String> getInputFiles() {
+        // TODO: Get file location from user
+        Scanner in = new Scanner(System.in);
+        char continue_char = 'n';
+
+        System.out.println("Please provide the full path of the file that you wish to execute\n");
+        while (fileLocationList.size() < 3 || continue_char == 'y') {
+            if (fileLocationList.size() < 3) {
+                String fileName = getFile(in, fileLocationList.size());
+            }
+            else {
+                System.out.println("Do you wish to enter another file? \n Enter (y) or (n): ");
+
+                continue_char = (char) in.nextByte();
+                if (continue_char == 'y') {
+                    String fileName = getFile(in, fileLocationList.size());
+                    if (StringUtils.isBlank(fileName) || fileName.equals("")) {
+                        return fileLocationList;
+                    } else {
+                        fileLocationList.add(fileName);
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
         return null;
+    }
+
+    public String getFile(Scanner in, int fileNumber) {
+        System.out.println("Enter file " + fileNumber + ": ");
+        String file = in.nextLine();
+
+        if ((StringUtils.isNotBlank(file) && fileLocationList.contains(file)) || (StringUtils.isBlank(file))) {
+            System.out.print("This file has already been provided.");
+
+            if (fileLocationList.size() < 3) {
+                System.out.print("Please enter a different file: ");
+                file = getFile(in, fileNumber);
+                System.out.println();
+            } else {
+                System.out.println("Do you wish to enter another file? \n Enter (y) or (n): ");
+
+                char continue_char = (char) in.nextByte();
+                if (continue_char == 'y') {
+                    file = getFile(in, fileNumber);
+                } else {
+                    return "";
+                }
+            }
+        } else {
+            return file;
+        }
+
+        return "";
     }
 
     @Override
