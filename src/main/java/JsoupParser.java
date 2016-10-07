07661f512b27de80d9ccf5cf7f17f0d7a057f916
kathy789 <ji79@purdue.edu>
2016-10-07 16:29:45 -0400

    send result to html

--- src/main/java/JsoupParser.java
+++ src/main/java/JsoupParser.java
@@ -1,12 +1,12 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
-import javax.swing.SwingUtilities;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
+//Prerequirement for html page : we need assign tag id="result".
+
 public class JsoupParser {
 	String filePath;  //the name of html to open and modify
 	Document doc; // // get the Document for the fileName, which is easy to access in any methods in this class.
@@ -22,36 +22,26 @@ public class JsoupParser {
 	public void displayResult(String result) throws IOException {
 		// Specify the Element form the document
 		 Element res = doc.getElementById("result");
-		 System.out.println(res.text()); // for debug purpose
+		 //System.out.println(res.text()); // for debug purpose
+		 
 		 // modify the text content of the Element
 		 res.text(result);
-		 System.out.println(res.text()); // for debug purpose
-		 // write back to the same file
-		
-		 SwingUtilities.invokeLater(new Runnable() {
-			    public void run() {
-			    	 FileWriter fw = null;
-					try {
-						fw = new FileWriter(filePath);
-						fw.write(doc.toString());
-						 fw.close();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				
-			    }
-			});
+		 //System.out.println(res.text()); // for debug purpose
+		 
+		 // write back to the same file		 
+    	 FileWriter fw = new FileWriter(filePath);
+		 fw.write(doc.toString());
+	     fw.close();
+	
 	}
 	
+	/*// usage example: 
 	public static void main(String[] args) throws IOException {
 	     
 	     JsoupParser jp = new JsoupParser("C:\\Users\\User\\eclipse\\JsoupParser\\src\\FrontEnd.html");
 	     jp.displayResult("test4 passed");
 		 
-	     JsoupParser jp2 = new JsoupParser("C:\\Users\\User\\eclipse\\JsoupParser\\src\\test.html");
-	     jp2.displayResult("test5 passed");
 		 
 	}
-
+    */
 }
